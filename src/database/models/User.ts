a6d4976a2a82d5b3e0edb5e4a38bd35a22a8748a
lucaskhliu <liukehong@gmail.com>
2023-02-28 04:16:09 +0800

    refactor(db): add isdemo flag to user model

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -6,25 +6,29 @@ interface ITimestamp {
 	updatedAt: Date;
 }
 
-const schema = new Schema({
-	//部署站点编码
-	site: String,
-	account: { type: String, index: true, required: true, lowercase: true },
-	//用户名，显示名，比如中文名字
-	username: { type: String, required: true },
-	//用户登录密码
-	password: { type: String, required: true },
-	// 手机验证码
-	phone: { type: String, unique: true, required: true },
-	// 填入手机时候，做个短信验证
-	phoneVerified: { type: Boolean, default: false },
-	// 微信授权登录的openid
-	openId: { type: String, default: "" },
-	// 微信授权登录的unionId
-	unionId: { type: String, default: "" },
-	// 最后登录的组织
-	tenant: { type: Schema.Types.ObjectId, ref: "Tenant" },
-});
+const schema = new Schema(
+	{
+		//部署站点编码
+		site: String,
+		account: { type: String, index: true, required: true, lowercase: true },
+		//用户名，显示名，比如中文名字
+		username: { type: String, required: true },
+		//用户登录密码
+		password: { type: String, required: true },
+		// 手机验证码
+		phone: { type: String, unique: true, required: true },
+		// 填入手机时候，做个短信验证
+		phoneVerified: { type: Boolean, default: false },
+		// 微信授权登录的openid
+		openId: { type: String, default: "" },
+		// 微信授权登录的unionId
+		unionId: { type: String, default: "" },
+		// 最后登录的组织
+		tenant: { type: Schema.Types.ObjectId, ref: "Tenant" },
+		demo: { type: Boolean, default: false },
+	},
+	{ timestamps: true },
+);
 schema.index({ site: 1, account: 1 }, { unique: true });
 
 export type UserType = HydratedDocument<InferSchemaType<typeof schema>> & ITimestamp;
